bde45bf974a69803af4be4789fb4d8418ecbd305
encrypted_bishal <bishalkharel0000@gmail.com>
2022-01-21 00:40:15 +0545

    added category,person,foodproduct models

--- eatfood/food_recomender/admin.py
+++ eatfood/food_recomender/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category,FoodProduct,Person
 
 admin.site.register(Category)
+admin.site.register(FoodProduct)
+admin.site.register(Person)
+
--- eatfood/food_recomender/models.py
+++ eatfood/food_recomender/models.py
@@ -1,7 +1,22 @@
 from django.db import models
 
+
+class Person(models.Model):
+    name=models.CharField(max_length=20),
+    birthday=models.DateField()
+
+    def __str__(self):
+        return self.name
+
 class Category(models.Model):
     name=models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
+
+class FoodProduct(models.Model):
+    food_name=models.CharField(max_length=100)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE,default=1)
+
+    def __str__(self):
+        return self.food_name
