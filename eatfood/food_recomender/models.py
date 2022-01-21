927587b92b89651552e45dc42dfa996f7b533a51
encrypted_bishal <bishalkharel0000@gmail.com>
2022-01-21 14:35:16 +0545

    Typos fixed in models

--- eatfood/food_recomender/models.py
+++ eatfood/food_recomender/models.py
@@ -1,24 +1,22 @@
 from django.db import models
 
-
 class Category(models.Model):
     name=models.CharField(max_length=200)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
 class FoodProduct(models.Model):
     food_name=models.CharField(max_length=100)
     category=models.ForeignKey(Category,on_delete=models.CASCADE,blank=True, null=True)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.food_name
 
 
 class Person(models.Model):
-    name=models.CharField(max_length=20),
-    #birthday=models.DateField(),
+    name=models.CharField(max_length=20)
     categories=models.ManyToManyField(Category)
     
-    def __unicode__(self):
+    def __str__(self):
         return self.name
